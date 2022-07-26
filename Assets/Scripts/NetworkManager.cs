6101d858298a774adde5db04204602fb96f8dabd
Oleg Kotov <onkotov@gmail.com>
2022-07-27 00:35:01 +0500

    Сделал меню с подключением к Photon

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -23,25 +23,17 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Connected to master server!");
-        CreateRoom("TestRoom");
-    }
-
     public void CreateRoom(string roomName)
     {
         PhotonNetwork.CreateRoom(roomName);
     }
-
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("Created room: " + PhotonNetwork.CurrentRoom.Name);
-    }
+        
     public void JoinRoom(string roomName)
     {
         PhotonNetwork.JoinRoom(roomName);
     }
+
+    [PunRPC]
     public void ChangeScene(string sceneName)
     {
         PhotonNetwork.LoadLevel(sceneName);
